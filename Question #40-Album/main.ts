f22ec13ceda1302c42f8ca53c4654be4dc5d011f
Mahnoor <160124465+Mahnoor-Tech@users.noreply.github.com>
2024-03-15 06:57:46 +0500

    Update main.ts

--- Question #40-Album/main.ts
+++ Question #40-Album/main.ts
@@ -1,23 +1,19 @@
 // Album: Write a function called make_album() that builds a Object describing a music album. The function should take in an artist name and an album title, and it should return a Object containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that Objects are storing the album information correctly. Add an optional parameter to make_album() that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object. Make at least one new function call that includes the number of tracks on an album.
 
-function make_album(
-  artist_name: string,
-  album_title: string
-): {
-  artist_name: string;
-  album_title: string;
-} {
-  const dictionaries = {
-    artist_name: artist_name.charAt(0).toUpperCase() + artist_name.slice(1),
-    album_title: album_title.charAt(0).toUpperCase() + album_title.slice(1),
-  };
-  return dictionaries;
+function make_album(artist: string, title: string, tracks?: number) {
+    let album = { artist, title };
+    if (tracks) {
+        album['tracks'] = tracks;
+    }
+    return album;
 }
 
-let album1 = make_album("Mahnoor", "Light");
-let album2 = make_album("Eric", "Abc");
-let album3 = make_album("John", "Xyz");
+// store function's output in a variable
+const album1 = make_album("Mahnoor", "My First Album");
+const album2 = make_album("Arman", "Never Loose your Hope");
+const album3 = make_album("Eric", "Productive Habits", 3);
 
+// display albums [artists, titles, tracks]
 console.log(album1);
 console.log(album2);
 console.log(album3);
