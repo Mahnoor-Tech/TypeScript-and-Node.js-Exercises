8bb650c593c2748e0e36b19ad3497fc93fd16d49
Mahnoor <160124465+Mahnoor-Tech@users.noreply.github.com>
2024-03-15 06:59:58 +0500

    Update main.ts

--- Question #41-Magicians/main.ts
+++ Question #41-Magicians/main.ts
@@ -2,7 +2,7 @@
 
 let magicians_names: string[] = ["John", "Eric", "Albert"];
 
-function show_magicians(magicians_names: string[]): void {
+function show_magicians(magicians_names: string[]) {
   for (let i = 0; i < magicians_names.length; i++) {
     console.log(magicians_names[i] + "\n");
   }
