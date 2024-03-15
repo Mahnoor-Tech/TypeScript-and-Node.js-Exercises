e0e6bf34d3a1da862f654be433d39a7e7e831cb3
Mahnoor <160124465+Mahnoor-Tech@users.noreply.github.com>
2024-03-15 07:20:57 +0500

    Update main.ts

--- Question #42-Great Magicians/main.ts
+++ Question #42-Great Magicians/main.ts
@@ -1,11 +1,21 @@
 // 42- Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
 
-let magicians_names: string[] = ["John", "Eric", "Albert"];
+let magicians_names: string[] = ["Mahnoor", "Erha", "Meerab"];
 
-function make_great(magicians_names: string[]): void {
+function show_magicians(magicians_names: string[]) {
   for (let i = 0; i < magicians_names.length; i++) {
-    console.log("The great magician" + " " + magicians_names[i] + ".\n");
+    console.log(magicians_names[i] + "\n");
   }
 }
 
+function make_great(magicians_names: string[]) {
+  for (let i = 0; i < magicians_names.length; i++) {
+    magicians_names[i] = `${magicians_names[i]}, "The Great"`;
+  }
+}
+
+// Update the original array with the added text 'the Great'
 make_great(magicians_names);
+
+// display the updated magician names
+show_magicians(magicians_names);  
