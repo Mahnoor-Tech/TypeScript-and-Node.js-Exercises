e90b8a3a1b4e04ca86a9a09ab5602f256b8077ee
Mahnoor <160124465+Mahnoor-Tech@users.noreply.github.com>
2024-03-16 08:12:00 +0500

    45 solutions of typescript questions

--- Question #45-Cars/main.ts
+++ Question #45-Cars/main.ts
@@ -1,26 +1,21 @@
 // 45- Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
 
-type car = {
-  manufacturer: string;
-  model: string;
-  [key: string]: any;
-};
-
-function car_info(
+function carDetails(
   manufacturer: string,
   model: string,
-  optional_feature: Record<string, any>
-): car {
-  return {
+  ...extra_feature: [string, any][]
+): Object {
+  // type any - now it will store anything
+  let myCar: any = {
     manufacturer,
     model,
-    ...optional_feature,
   };
+
+  extra_feature.forEach(([key, value]) => {
+    myCar[key] = value;
+  });
+
+  return myCar;
 }
 
-let car_details: car = car_info("Tesla", "Model S", {
-  color: "white",
-  year: "2024",
-});
-
-console.log(car_details);
+console.log(carDetails("BMW", "X5", ["color", "Alpine White"], ["year", 2024]));
