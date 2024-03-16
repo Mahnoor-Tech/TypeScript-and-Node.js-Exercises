aee7d480e9355d0efb93ce3ce931bc34c8164387
Mahnoor <160124465+Mahnoor-Tech@users.noreply.github.com>
2024-03-16 07:33:43 +0500

    45 solutions of typescript questions

--- Question #43-Unchanged Magicians/main.ts
+++ Question #43-Unchanged Magicians/main.ts
@@ -1,26 +1,34 @@
 // 43- Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
 
-let magicians_names: string[] = ["John", "Eric", "Albert"];
+let magicians: string[] = ["Mahnoor", "Erha", "Meerab"];
 
-function show_magicians(magicians_names: string[]): void {
-  for (let i = 0; i < magicians_names.length; i++) {
-    console.log(magicians_names[i] + "\n");
-  }
+/*
+First call: returns magician names from 'magicians' array. 
+["Mahnoor", "Erha", "Meerab"]
+Second call: returns names from 'greatMagicians' array. 
+[ 'Mahnoor the Great', 'Erha the Great', 'Meerab the Great' ];
+*/
+
+function show_magicians(items: string[]) {
+  items.map((magician) => {
+    console.log(magician);
+  });
 }
 
-function make_great2(magicians_names: string[]): string[] {
-  let great_magician: string[] = [];
-
-  for (let i = 0; i < magicians_names.length; i++) {
-    great_magician.push(
-      "The great magician" + " " + magicians_names[i] + ".\n"
-    );
-  }
-  return great_magician;
+function make_great(magicians: string[]): string[] {
+  return magicians.map((item) => `${item} the Great`);
 }
 
-let magician3: string[] = ["Bob", "Crist", "Briz"];
-let great_magician2: string[] = make_great2(magician3);
+// Using Spread Operator To Create A Copy Of An Array | Can Also Use slice();
+let greatMagicians = make_great([...magicians]);
+// console.log(greatMagicians);
 
-show_magicians(magician3);
-show_magicians(great_magician2);
+// Original Array | ["Mahnoor", "Erha", "Meerab"]
+console.log("Original Array:");
+console.log(`---------------`);
+show_magicians(magicians);
+
+// Modified Array | [ 'Mahnoor the Great', 'Erha the Great', 'Meerab the Great' ];
+console.log(`\nModified Array - Great Magicians:`);
+console.log(`------------------------------`);
+show_magicians(greatMagicians);
